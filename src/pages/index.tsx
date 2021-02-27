2d99a4ec4ae7a1a5fbba5e1b3332553325023135
Tarsis Santos <tarsissan@gmail.com>
2021-02-26 21:47:35 -0300

    codigos da aula 5: criando um modal que aparece quando o usuario sobe de nivel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from  'next/head'
 import styles from '../styles/pages/Home.module.css'
+import { GetServerSideProps } from 'next'
 
 import { Profile } from '../components/Profile'
 import { Countdown } from '../components/Countdown'
@@ -7,29 +8,57 @@ import { ChallengeBox } from '../components/ChallengeBox'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { CountdownProvider } from '../contexts/CountdownContext'
 import { CompletedChallenges } from '../components/CompletedChallenges'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
-export default function Home() {
+interface HomeProps{
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props) {
+  console.log(props)
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | Have a Break</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
 
-          <div>
-            <ChallengeBox />
-          </div>
+      <div className={styles.container}>
+        <Head>
+          <title>Início | Have a Break</title>
+        </Head>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
 
-        </section>
-      </CountdownProvider>
-    </div>
+            <div>
+              <ChallengeBox />
+            </div>
+
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps:GetServerSideProps = async(ctx) => {
+  
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+
+  return{
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
